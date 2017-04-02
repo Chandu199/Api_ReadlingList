be41bbd2df01b89ee9ca2d134ad6399b1a19ce07
Chandrakanth N <CodeToLive@Chandrakanths-MacBook-Pro.local>
2017-04-02 16:19:49 +0530

    add gene to the model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170402094000) do
+ActiveRecord::Schema.define(version: 20170402104927) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20170402094000) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.time     "finished_at"
+    t.string   "author"
+    t.text     "review"
+    t.integer  "genre_id"
+    t.string   "amazon_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- test/integration/creating_book_test.rb
+++ test/integration/creating_book_test.rb
@@ -6,6 +6,11 @@ class CreatingBookTest < ActionDispatch::IntegrationTest
 		post '/books',{book: {
 			title: 'Pragmatic Programmer',
 			rating: 3,
+			author: "Dave thomas",
+			genre_id: 1,
+			review: "An Awesome book.",
+			amazon_id: "12344"
+
 			}}.to_json,
 			{'Accept' =>'application/json','Content-Type'=>'application/json'}
 		assert_equal 201,response.status
@@ -14,6 +19,11 @@ class CreatingBookTest < ActionDispatch::IntegrationTest
 		assert_equal book_url(book[:id]),response.location
 		assert_equal 'Pragmatic Programmer', book[:title]
 		assert_equal 3, book[:rating]
+		assert_equal 'Dave thomas', book[:author]
+		assert_equal 1, book[:genre_id]
+		assert_equal 'An Awesome book.', book[:review]
+		assert_equal "12344", book[:amazon_id]
+
 	end
 
 	test 'Create books with invalid data' do
